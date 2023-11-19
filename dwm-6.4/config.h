271e545aef7fae82ef1a1646cbc181d16d7579de
Adrien <139459320+merlusine@users.noreply.github.com>
2023-11-19 13:13:10 +0100

    Update config.h

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *scrotcmd[]  = { "scrot", "/home/adrien/Pictures/Screenshots/screenshot.jpg", NULL };
+static const char *scrotcmd[]  = { "scrot", "~/Pictures/Screenshots/screenshot.jpg", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
